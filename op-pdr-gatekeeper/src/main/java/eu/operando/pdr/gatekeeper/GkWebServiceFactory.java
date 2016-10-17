184d13a021f40d053b5ab43e8d51d05336b4da48
Matthew Gallagher <Matt.Gallagher@oxfordcc.co.uk>
2016-10-17 10:16:30 +0100

    configurable API details & Jenkins support

--- op-pdr-gatekeeper/src/main/java/eu/operando/pdr/gatekeeper/GkWebServiceFactory.java
+++ op-pdr-gatekeeper/src/main/java/eu/operando/pdr/gatekeeper/GkWebServiceFactory.java
@@ -1,6 +1,7 @@
 package eu.operando.pdr.gatekeeper;
 
 import eu.operando.CredentialsOperando;
+import eu.operando.Utils;
 import eu.operando.moduleclients.ClientAuthenticationApiOperandoClient;
 import eu.operando.moduleclients.ClientAuthenticationApiOperandoService;
 import eu.operando.moduleclients.ClientDataAccessNode;
@@ -9,24 +10,33 @@ import eu.operando.moduleclients.http.HttpRequestBuilderAuthenticationApi;
 
 public class GkWebServiceFactory
 {
+	// Location of properties file.
+	private static final String PROPERTIES_FILE_GK = "config.properties";
+
+	// Property file property names.
+	private static final String PROPERTY_NAME_ORIGIN_AUTHENTICATION_API = "originAuthenticationApi";
+	private static final String PROPERTY_NAME_ORIGIN_DATA_ACCESS_NODE = "originDataAccessNode";
+	private static final String PROPERTY_NAME_USERNAME_GK = "usernameGk";
+	private static final String PROPERTY_NAME_PASSWORD_GK = "passwordGk";
+
 	private GkWebServiceFactory()
 	{
 		// Factory with static method for creating GkWebServices. Should not be instantiated.
 	}
-	
+
 	public static GkWebService create()
 	{
-		String originAuthenticationService = "https://snf-706921.vm.okeanos.grnet.gr:8443";
-		String originDataAccessNode = "http://localhost:8080";
-		ClientAuthenticationApiOperandoService clientAuthenticationApiOperandoService = new ClientAuthenticationApiOperandoService(originAuthenticationService);
-		
-		String usernameGk = "operando";
-		String passwordGk = "1234";
+		String originAuthenticationApi = Utils.loadPropertyString(PROPERTIES_FILE_GK, PROPERTY_NAME_ORIGIN_AUTHENTICATION_API);
+		String originDataAccessNode = Utils.loadPropertyString(PROPERTIES_FILE_GK, PROPERTY_NAME_ORIGIN_DATA_ACCESS_NODE);
+		ClientAuthenticationApiOperandoService clientAuthenticationApiOperandoService = new ClientAuthenticationApiOperandoService(originAuthenticationApi);
+
+		String usernameGk = Utils.loadPropertyString(PROPERTIES_FILE_GK, PROPERTY_NAME_USERNAME_GK);
+		String passwordGk = Utils.loadPropertyString(PROPERTIES_FILE_GK, PROPERTY_NAME_PASSWORD_GK);
 		CredentialsOperando credentials = new CredentialsOperando(usernameGk, passwordGk);
-		RequestBuilderAuthenticationApi requestBuilderAuthenticatoinApi = new HttpRequestBuilderAuthenticationApi(originAuthenticationService, credentials);
+		RequestBuilderAuthenticationApi requestBuilderAuthenticatoinApi = new HttpRequestBuilderAuthenticationApi(originAuthenticationApi, credentials);
 		ClientAuthenticationApiOperandoClient clientAuthenticationServiceOperandoClient = new ClientAuthenticationApiOperandoClient(requestBuilderAuthenticatoinApi);
 		ClientDataAccessNode clientDataAccessNode = new ClientDataAccessNode(originDataAccessNode, clientAuthenticationServiceOperandoClient);
-		
+
 		return new GkWebServiceImpl(clientAuthenticationApiOperandoService, clientDataAccessNode);
 	}
 }
