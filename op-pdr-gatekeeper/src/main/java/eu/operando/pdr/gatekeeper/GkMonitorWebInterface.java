a43e955da78cfe6a828dffdf8d7457e341debf5e
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-26 16:18:32 +0200

    added safe method to gk to be able to monitor

---
+++ op-pdr-gatekeeper/src/main/java/eu/operando/pdr/gatekeeper/GkMonitorWebInterface.java
@@ -0,0 +1,33 @@
+package eu.operando.pdr.gatekeeper;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+
+/**
+ * Due to changes in the work-flow for handling requests to access data, the Gatekeeper is now essentially a placeholder module (which will manage
+ * additional up-front validation of requests beyond checking authentication), which currently functions as a proxy to the Data Access Node.
+ * 
+ */
+@Path("monitor")
+public class GkMonitorWebInterface
+{
+	private GkWebService service = GkWebServiceFactory.create(); 
+
+	@GET
+	public Response processGetRequest(@Context HttpHeaders headers)
+	{
+		System.out.print("monitor"); //GBE
+		Response response = null;
+		
+        return Response.ok().build();
+	}
+	
+}
