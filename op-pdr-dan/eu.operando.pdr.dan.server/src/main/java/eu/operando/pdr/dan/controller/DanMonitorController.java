17a7c542c902168c5260ab270101de887cd02eeb
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-27 12:05:40 +0200

    Tries to add monitoring
    May fail due to RequestMapping precedence

---
+++ op-pdr-dan/eu.operando.pdr.dan.server/src/main/java/eu/operando/pdr/dan/controller/DanMonitorController.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *  # Copyright (c) 2016 {UPRC}.
+ *  # All rights reserved. This program and the accompanying materials
+ *  # are made available under the terms of the The MIT License (MIT).
+ *  # which accompanies this distribution, and is available at
+ *  # http://opensource.org/licenses/MIT
+ *
+ *  # Contributors:
+ *  #    {Constantinos Patsakis} {UPRC}
+ *  #    {Stamatis Glykos} {UPRC}
+ *  #    {Constantinos Alexandris} {UPRC}
+ *  # Initially developed in the context of OPERANDO EU project www.operando.eu 
+ *******************************************************************************/
+
+package eu.operando.pdr.dan.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/monitor")
+public class DanController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			response.ok().entity("server ok").build();
+			return;
+		} catch (IOException ioe) {
+			LOGGER.error(ioe.getMessage());
+		}
+
+	}
+}
